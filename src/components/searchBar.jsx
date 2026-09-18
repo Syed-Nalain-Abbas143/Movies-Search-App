d9b167751d84f1232f1e77ff16b91f6765916a6c
Syed Nalain Abbas <nainu1269@gmail.com>
2026-09-18 14:39:41 +0500

    Changed styling

--- src/components/searchBar.jsx
+++ src/components/searchBar.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const searchBar = ({ title, setTitle, Loading, displayMovies }) => {
   return (
-     
+    <>
     <input
         type="text"
         placeholder="Search for a movie..."
         value={title}
-        className="px-5 py-3.5 rounded-xl bg-white text-gray-900 placeholder-gray-500 border border-gray-300 shadow-lg outline-none transition focus:border-blue-400 focus:ring-4 focus:ring-blue-400/30"
+        className="px-5 py-2 rounded-xl text-gray-300 border-gray-400 border placeholder-gray-500 shadow-lg outline-none "
         onChange={(e) => {
           setTitle(e.target.value);
         }}
@@ -17,17 +17,7 @@ const searchBar = ({ title, setTitle, Loading, displayMovies }) => {
           }
         }}
       />
-
-      // <button
-      //   disabled={Loading}
-      //   className="px-6 py-2 text-lg rounded-full bg-amber-500 text-gray-900 font-bold shadow-lg transition-all duration-300 hover:bg-amber-600 hover:scale-95 cursor-pointer active:scale-95 disabled:bg-gray-400 disabled:text-gray-100"
-      //   onClick={() => {
-      //     displayMovies(title);
-      //   }}
-      // >
-      //   Search
-      // </button>
-    
+      </> 
   );
 };
 
